580b322bb4767d175ecabd42a4fe6d7b2aba6677
holmgr <viktor.holmgren@gmail.com>
2016-11-20 21:22:21 +0100

    Implement a FIFO queue in Rust :)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,6 @@
+// Add a mod entry for the implementation below so that tests can be found by cargo
+mod queue;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello!, Your rust setup seems to be working :)");
 }
