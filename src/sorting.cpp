d96211f9b011eb2c4ce231acf8f04fb529acb29a
Alexander Sääf <alesa601@student.liu.se>
2017-03-11 08:00:46 +0100

    Format files using clangformat

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -16,39 +16,40 @@ void insertion_sort(std::vector<int> &vec) {
   }
 }
 
-//Sorts the given input using in-place quick sort.
-//Should be called as quick_sort(A, 0, A.size())
-void quick_sort(std::vector<int> &vec, int lo, int hi){
-  if(lo < hi){
-    //Partition the input between lo and hi
+// Sorts the given input using in-place quick sort.
+// Should be called as quick_sort(A, 0, A.size())
+void quick_sort(std::vector<int> &vec, int lo, int hi) {
+  if (lo < hi) {
+    // Partition the input between lo and hi
     int p = partition(vec, lo, hi);
-    //Sort the list to the left and to the right of the pivot p
+    // Sort the list to the left and to the right of the pivot p
     quick_sort(vec, lo, p);
     quick_sort(vec, p + 1, hi);
   }
 }
 
-//Helper function for quick_sort.
-//Partitions the input between lo and hi, using lo as pivot
-int partition(std::vector<int> &vec, int lo, int hi){
-  //We pick the value at lo as pivot
+// Helper function for quick_sort.
+// Partitions the input between lo and hi, using lo as pivot
+int partition(std::vector<int> &vec, int lo, int hi) {
+  // We pick the value at lo as pivot
   int pivot = vec[lo];
-  //Create two search-heads
+  // Create two search-heads
   int i = lo - 1;
   int j = hi;
-  while(true){
-    //If the element at the search-head does not need to move, move the head
+  while (true) {
+    // If the element at the search-head does not need to move, move the head
     do {
       i++;
-    } while(vec[i] < pivot);
+    } while (vec[i] < pivot);
     do {
       j--;
-    } while(vec[j] > pivot);
-    //Our search-heads have collided => We have gone through the entire interval
-    if(i >= j){
+    } while (vec[j] > pivot);
+    // Our search-heads have collided => We have gone through the entire
+    // interval
+    if (i >= j) {
       return j;
     }
-    //Swap the elements at the heads
+    // Swap the elements at the heads
     int tmp = vec[i];
     vec[i] = vec[j];
     vec[j] = tmp;
