461323b2642243b6b1fd714a94baf18181de083f
Alexander Sääf <alexandersaaf95@gmail.com>
2017-03-11 19:02:21 +0100

    Implement Quick sort  (#5)
    * Implement quicksort
    
    * Format files using clangformat
    
    * Update sorting.cpp

--- src/sorting.h
+++ src/sorting.h
@@ -5,4 +5,8 @@
 void insertion_sort(std::vector<int> &vec);
 void merge_sort(std::vector<int> &vec);
 
+void quick_sort(std::vector<int> &vec, int lo, int hi);
+
+int partition(std::vector<int> &vec, int lo, int hi);
+
 #endif /* ifndef SORTING_H */
--- test/sorting_unittest.cpp
+++ test/sorting_unittest.cpp
@@ -66,3 +66,36 @@ TEST(MergeSortTest, Mixed) {
     EXPECT_EQ(vec_sorted[i], vec[i]);
   }
 }
+
+
+// Tests quick_sort()
+
+// Tests empty input.
+TEST(QuickSortTest, Empty) {
+  std::vector<int> empty_vec;
+
+  quick_sort(empty_vec, 0, empty_vec.size());
+  EXPECT_TRUE(empty_vec.empty());
+}
+
+// Tests non empty positive input.
+TEST(QuickSortTest, Positive) {
+  std::vector<int> vec = {3, 4, 17, 18, 15};
+  std::vector<int> vec_sorted = {3, 4, 15, 17, 18};
+
+  quick_sort(vec, 0, vec.size());
+  for (int i = 0; i < vec.size(); i++) {
+    EXPECT_EQ(vec_sorted[i], vec[i]);
+  }
+}
+
+// Tests mixed input.
+TEST(QuickSortTest, Mixed) {
+  std::vector<int> vec = {-3, 4, -17, 18, 15};
+  std::vector<int> vec_sorted = {-17, -3, 4, 15, 18};
+
+  quick_sort(vec, 0, vec.size());
+  for (int i = 0; i < vec.size(); i++) {
+    EXPECT_EQ(vec_sorted[i], vec[i]);
+  }
+}
