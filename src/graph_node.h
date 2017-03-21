fdc87c1bcf4d43e97eab369781d802fb12c40b83
Philip Johansson <philip_yeah@hotmail.com>
2017-03-21 20:53:51 +0100

    Fix the template problem.

--- src/graph_node.h
+++ src/graph_node.h
@@ -8,16 +8,51 @@ template<typename V>
 class GraphNode {
 private:
 	V value;
-	std::vector<GraphNode*> neighbors;
+	std::vector<GraphNode<V>*> neighbors;
 public:
-	void setValue(const V new_value);
-	V getValue() const;
-	std::vector<GraphNode*> getNeighbors() const;
+	void setValue(V new_value);
+	V getValue();
+	std::vector<GraphNode<V>*> getNeighbors() const;
 
-	void addNeighbor(GraphNode* node);
+	void addNeighbor(GraphNode<V>* node);
 	/*
 	It is expected that the user of this structure will get the child befor removing it.
 	If the index is above the given size of the childrenvector the nullptr will be returned.
 	*/
 	void removeNeighbor(int index);
 };
+
+/* --------------------------------------------- ## ---------------------------------------------*/
+
+/*
+This is the implementation of a graphnode and will follow the expectaions of the headerfile,
+that is writen for the functions.
+*/
+
+template<typename V>
+void GraphNode<V>::setValue(V new_value) {
+	value = new_value;
+}
+
+template<typename V>
+V GraphNode<V>::getValue() {
+	return value;
+}
+
+template<typename V>
+std::vector<GraphNode<V>*> GraphNode<V>::getNeighbors() const {
+	return neighbors;
+}
+
+template<typename V>
+void GraphNode<V>::addNeighbor(GraphNode* node) {
+	neighbors.push_back(node);
+}
+
+
+template<typename V>
+void GraphNode<V>::removeNeighbor(int index) {
+	if (index < neighbors.size() && index >= 0) {
+		neighbors.erase(neighbors.begin() + index);
+	}
+}
--- src/tree_node.h
+++ src/tree_node.h
@@ -4,11 +4,11 @@
 This class is a node for a tree structure with a parentpointer and
 unlimited number of children so it is not limited to a binary-tree.
 */
-template<typename V>
+template<class V>
 class TreeNode {
 private:
 	V value;
-	TreeNode<V>* parent;
+	TreeNode<V>* parent = nullptr;
 	std::vector<TreeNode<V>*> children;
 public:
 	void setValue(const V new_value);
@@ -28,3 +28,55 @@ public:
 	*/
 	void removeChild(int index);
 };
+
+/* --------------------------------------------- ## ---------------------------------------------*/
+
+/*
+This is the implementation of a treenode and will follow the expectaions of the headerfile,
+that is writen for the functions.
+*/
+
+
+template<typename V>
+void TreeNode<V>::setValue(const V new_value) {
+	value = new_value;
+}
+
+template<typename V>
+V TreeNode<V>::getValue() {
+	return value;
+}
+
+template<typename V>
+std::vector<TreeNode<V>*> TreeNode<V>::getChildren() {
+	return children;
+}
+
+template<typename V>
+void TreeNode<V>::addChild(TreeNode<V>* node) {
+	children.push_back(node);
+}
+
+
+template<typename V>
+void TreeNode<V>::removeChild(int index) {
+	// If the index is above the size of the list of children then return nullptr.
+	if (index < children.size() && index >= 0) {
+		children.erase(children.begin() + index);
+	}
+}
+
+template<typename V>
+TreeNode<V>* TreeNode<V>::getParent() {
+	return parent;
+}
+
+template<typename V>
+void TreeNode<V>::setParent(TreeNode<V>* node) {
+	parent = node;
+}
+
+template<typename V>
+void TreeNode<V>::removeParent() {
+	parent = nullptr;
+}
